ccfddb0561cf1253d0837bd618e49a2f9722c428
M*C*O <mcofficer@gmx.de>
2019-03-30 22:05:57 +0100

    ...Actual initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-es-plugin-manager
+# es-plugin-manager (ESPIM)
+A Proof of concept client for [endless-sky-plugins](https://github.com/MCOfficer/endless-sky-plugins).
---
+++ src/main.rs
@@ -0,0 +1,124 @@
+extern crate app_dirs;
+extern crate clap;
+extern crate git2;
+extern crate remove_dir_all;
+
+use remove_dir_all::remove_dir_all;
+use std::path::Path;
+use std::path::PathBuf;
+
+use app_dirs::*;
+use clap::{App, Arg, SubCommand};
+use git2::ObjectType;
+use git2::Repository;
+use git2::Signature;
+
+const VERSION: &str = "0.1.0";
+const REPO_URL: &str = "https://github.com/MCOfficer/endless-sky-plugins.git";
+const APP_INFO: AppInfo = AppInfo{name: "ESPIM", author: "MCOfficer"};
+
+fn main() {
+    let matches = App::new("Endless Sky Plug-In Manager")
+        .version(VERSION)
+        .about("A Proof-of-Concept Plug-In Manager for Endless Sky.")
+        .subcommand(SubCommand::with_name("init")
+            .about("Clones the Plug-In repository. Use before anything else.")
+        )
+        .subcommand(SubCommand::with_name("update")
+            .about("Updates the local Plug-In repository.")
+        )
+        .subcommand(SubCommand::with_name("list")
+            .about("Lists all available plug-ins")
+        )
+        .arg(Arg::with_name("verbose")
+            .long("verbose")
+            .short("v")
+            .help("Prints moar info.")
+        ).get_matches();
+
+    let verbose = matches.is_present("verbose");
+    if verbose {
+        println!("Verbose Output enabled.");
+        println!("ESPIM v{}", VERSION);
+    }
+
+    if let Some(matches) = matches.subcommand_matches("init") {
+        init(verbose);
+    }
+    else if let Some(matches) = matches.subcommand_matches("update") {
+        update(verbose);
+    }
+    else if let Some(matches) = matches.subcommand_matches("list") {
+        list(verbose);
+    }
+}
+
+fn get_repo_dir(verbose: bool) -> PathBuf {
+    let repo_dir = match get_app_dir(AppDataType::UserCache, &APP_INFO, "repo") {
+        Ok(repo_dir) => repo_dir,
+        Err(e) => panic!("app_dirs failed with an error: {}", e),
+    };
+    if verbose {
+        println!("app_dirs returned {} as repo directory", repo_dir.to_string_lossy());
+    }
+    repo_dir
+}
+
+fn init(verbose: bool) {
+    let repo_dir = get_repo_dir(verbose);
+    if repo_dir.exists() {
+        println!("Repo Directory already exists. Did you run this command before?");
+        return;
+    }
+    if verbose {
+        println!("Cloning {} into {}", REPO_URL, repo_dir.to_string_lossy());
+    }
+
+    let repo = match Repository::clone(REPO_URL, repo_dir) {
+        Ok(repo) => repo,
+        Err(e) => panic!("Failed to Clone Repository: {}", e),
+    };
+
+    println!("Done.")
+}
+
+fn update(verbose: bool) {
+    let repo_dir = get_repo_dir(verbose);
+    if !repo_dir.exists() && verbose {
+        println!("Repo directory does not exist.");
+        return;
+    }
+    if verbose {
+        println!("This actually removes the current repo and re-clones it. Sue me.");
+        println!("Removing directory {}", repo_dir.to_string_lossy())
+    }
+
+    match remove_dir_all(repo_dir) {
+        Ok(o) => o,
+        Err(e) => panic!("Failed to remove the repo directory: {}", e)
+    };
+    init(verbose);
+}
+
+fn list(verbose: bool) {
+    let repo_dir = get_repo_dir(verbose);
+    if !repo_dir.exists() && verbose {
+        println!("Repo directory does not exist.");
+        return;
+    }
+    if verbose {
+        ("Opening Repository {}", repo_dir.to_string_lossy());
+    };
+
+    let repo = match Repository::open(repo_dir.as_path()) {
+        Ok(repo) => repo,
+        Err(e) => panic!("Failed to open Repository: {}", e),
+    };
+    let submodules = match repo.submodules() {
+        Ok(repo) => repo,
+        Err(e) => panic!("Failed to load Submodules: {}", e),
+    };
+    for submodule in &submodules {
+        println!("- {}", submodule.name().unwrap());
+    }
+}
